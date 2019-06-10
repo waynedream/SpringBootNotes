b75d8429dcd3a906d54157b4623c7fb5af291d9d
waynedream <wgq9527@foxmail.com>
2019-06-10 20:58:44 +0800

    解决静态资源无法访问的问题

--- race/src/main/java/com/wayne/race/pojo/Employee.java
+++ race/src/main/java/com/wayne/race/pojo/Employee.java
@@ -1,8 +1,5 @@
 package com.wayne.race.pojo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.util.Date;
 
 public class Employee {
